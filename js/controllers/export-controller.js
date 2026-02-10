f5f1dd2391c98979440ad69d07252286502d0779
Constantinescu Cristian-Valentin <constantinescu.cristian@gmail.com>
2026-02-10 15:31:07 +0200

    Remove unused controller wrappers and simplify bootstrap scripts

--- js/controllers/export-controller.js
+++
@@ -1,9 +0,0 @@
-(function initExportController(global) {
-    global.DSExportController = {
-        generateTeamAssignments: global.generateTeamAssignments,
-        openDownloadModal: global.openDownloadModal,
-        closeDownloadModal: global.closeDownloadModal,
-        downloadTeamMap: global.downloadTeamMap,
-        downloadTeamExcel: global.downloadTeamExcel,
-    };
-})(window);
